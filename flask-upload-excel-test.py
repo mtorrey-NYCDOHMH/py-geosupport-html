a4c4450f2dbc6b44ea1558c76ef8534fc7cefff4
Mark Torrey <mtorrey@health.nyc.gov>
2025-05-08 10:12:47 -0400

    changed url for flask app to staging server

--- flask-upload-excel-test.py
+++ flask-upload-excel-test.py
@@ -258,5 +258,7 @@ def end_session():
 
 # This is necessary to run this file on the command line. You can turn off debugging, or comment this whole thing if it is run from Apache
 if __name__ == "__main__":
-    app.run(debug=True)
+    #app.run(debug=True) # by default, runs on localhost:5000
+    # If you want to run for remote system, uncomment:
+    app.run(host='stgapprcode.health.dohmh.nycnet', port=5000
 
